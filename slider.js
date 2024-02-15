f8fdf87ef1cb1494ac262bf6a5f3dac97ff9fa0e
lidrail35 <lidrail35@mail.ru>
2024-02-15 22:19:34 +0300

    feat: delete unused js-code, add move to right

--- slider.js
+++ slider.js
@@ -1,37 +1,22 @@
 
 console.log('hello');
-// const pathToImage = './assets/img/';
-// const arrImage = ['pict-1.jpg', 
-//                   'pict-2.jpg', 
-//                   'pict-3.jpg', 
-//                   'pict-4.jpg', 
-//                   'pict-5.jpg'];
-
-let nowImage = 0;
-const totalImage = 5;
-
-const optionImage = document.querySelector('.option-image');
-let imageWidth = optionImage.clientWidth;
 
 const imageContainer = document.querySelector('.image-container')
 const leftArrow = document.querySelector('.array-left');
 const rightArrow = document.querySelector('.array-right');
-// const imageContainer = document.querySelector('.visible-image');
-
 
 leftArrow.addEventListener('click', ()=> {
-  (nowImage === 0) ? nowImage = totalImage : nowImage--;
-  setImage(nowImage);
+  const lastChild = imageContainer.removeChild(imageContainer.children[imageContainer.children.length - 1]);
+   imageContainer.children[1].classList.toggle('center', false);
+   imageContainer.children[1].classList.toggle('rightBlock', true);
+   imageContainer.children[0].classList.toggle('leftBlock', false);
+   imageContainer.children[0].classList.toggle('center', true);
+
+   imageContainer.insertBefore(lastChild, imageContainer.children[0]);
+   imageContainer.children[0].classList.toggle('rightBlock', false);
+   imageContainer.children[0].classList.toggle('leftBlock', true);
 });
 
  rightArrow.addEventListener('click', ()=> {
-   (nowImage === totalImage) ? nowImage = 0 : nowImage++
-   setImage(nowImage);
+   
  });
-
- const setImage = nowImage => {
-  console.log(nowImage);
-  imageContainer.setAttribute('style', `transform: translateX(${- nowImage * imageWidth}px)`)
-//   imageContainer.src = pathToImage + arrImage[nowImage];
-//   console.log(nowImage);
-}
